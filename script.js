bdcd759ce2e028d0ab56d6fe2d09710e189b51e3
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-21 12:30:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const makeChange = (c) => {
   // your name here
 	let output=[];
-	let units={q:25,d:10,n:5,p:1}
+	 // let num = parseInt(c); // Corrected ParseInt to parseInt
+  let units = { q: 25, d: 10, n: 5, p: 1 };
 	for(let i in units){
-		let curr=units;
-		output[i]=ParseInt(num/curr);
+		let curr=units[i];
+		output[i]=parseInt(num/curr);
 		num=num-curr*output[i]
 	}
 	return output;
+
 	
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
